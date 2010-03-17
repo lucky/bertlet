3feb55e1798663c7d69e8119afcd25c3c0412de2
Jared Kuolt <luckythetourist@gmail.com>
2010-03-17 11:23:41 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+version = '0.2'
+
+setup(
+    name='bertlet',
+    version=version,
+    description='BERT-RPC using Eventlet',
+    author='Jared Kuolt',
+    author_email='luckythetourist@gmail.com',
+    url='http://github.com/luckythetourist/bertlet',
+    packages=['bertlet'],
+    license='MIT License',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python', 
+        'Topic :: Software Development', 
+        'Topic :: Software Development :: Libraries', 
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
